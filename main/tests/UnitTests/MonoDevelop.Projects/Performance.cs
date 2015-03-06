81173bd73c9d1ecb13020759beb832c44ba14f0c
David Karlaš <david.karlas@xamarin.com>
2015-03-06 15:53:56 +0100

    test3

--- main/tests/UnitTests/MonoDevelop.Projects/Performance.cs
+++ main/tests/UnitTests/MonoDevelop.Projects/Performance.cs
@@ -41,15 +41,127 @@ using System.Collections.Generic;
 using Microsoft.CodeAnalysis.Diagnostics;
 using System.Collections.Immutable;
 using System.Diagnostics;
+using System.Collections;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
+using System.ComponentModel;
 
 namespace MonoDevelop.Projects
 {
 	public class Performance : TestBase
 	{
+
+		public class ProcessInfo
+		{
+			public ArrayList threadInfoList = new ArrayList ();
+			public int basePriority;
+			public string processName;
+			public int processId;
+			public int handleCount;
+			public long poolPagedBytes;
+			public long poolNonpagedBytes;
+			public long virtualBytes;
+			public long virtualBytesPeak;
+			public long workingSetPeak;
+			public long workingSet;
+			public long pageFileBytesPeak;
+			public long pageFileBytes;
+			public long privateBytes;
+			public int mainModuleId; // used only for win9x - id is only for use with CreateToolHelp32
+			public int sessionId;
+		}
+
+		public class ThreadInfo
+		{
+			public int threadId;
+			public int processId;
+			public int basePriority;
+			public int currentPriority;
+			public IntPtr startAddress;
+			public ThreadState threadState;
+#if !FEATURE_PAL
+			public ThreadWaitReason threadWaitReason;
+#endif // !FEATURE_PAL
+		}
+
+		public static ThreadInfo [] GetProcessInfos ()
+		{
+			int p = (int)Environment.OSVersion.Platform;
+			if ((p == 4) || (p == 6) || (p == 128)) {
+				return new ThreadInfo [0];
+			}
+			IntPtr handle = (IntPtr)(-1);
+			GCHandle bufferHandle = new GCHandle ();
+			var threadInfos = new List<ThreadInfo> ();
+			Hashtable processInfos = new Hashtable ();
+
+			int processId = Process.GetCurrentProcess ().Id;
+			try {
+				handle = NativeMethods.CreateToolhelp32Snapshot (NativeMethods.TH32CS_SNAPPROCESS | NativeMethods.TH32CS_SNAPTHREAD, 0);
+				if (handle == (IntPtr)(-1)) throw new Win32Exception ();
+				int entrySize = (int)Marshal.SizeOf (typeof(NativeMethods.WinProcessEntry));
+				int bufferSize = entrySize + NativeMethods.WinProcessEntry.sizeofFileName;
+				int [] buffer = new int [bufferSize / 4];
+				bufferHandle = GCHandle.Alloc (buffer, GCHandleType.Pinned);
+				IntPtr bufferPtr = bufferHandle.AddrOfPinnedObject ();
+				Marshal.WriteInt32 (bufferPtr, bufferSize);
+
+				HandleRef handleRef = new HandleRef (null, handle);
+
+				if (NativeMethods.Process32First (handleRef, bufferPtr)) {
+					do {
+						NativeMethods.WinProcessEntry process = new NativeMethods.WinProcessEntry ();
+						Marshal.PtrToStructure (bufferPtr, process);
+						ProcessInfo processInfo = new ProcessInfo ();
+						String name = Marshal.PtrToStringAnsi ((IntPtr)((long)bufferPtr + entrySize));
+						processInfo.processName = Path.ChangeExtension (Path.GetFileName (name), null);
+						processInfo.handleCount = process.cntUsage;
+						processInfo.processId = process.th32ProcessID;
+						processInfo.basePriority = process.pcPriClassBase;
+						processInfo.mainModuleId = process.th32ModuleID;
+						processInfos.Add (processInfo.processId, processInfo);
+						Marshal.WriteInt32 (bufferPtr, bufferSize);
+					}
+					while (NativeMethods.Process32Next (handleRef, bufferPtr));
+				}
+
+				NativeMethods.WinThreadEntry thread = new NativeMethods.WinThreadEntry ();
+				thread.dwSize = Marshal.SizeOf (thread);
+				if (NativeMethods.Thread32First (handleRef, thread)) {
+					do {
+						if(processId != thread.th32OwnerProcessID)
+							continue;
+						ThreadInfo threadInfo = new ThreadInfo ();
+						threadInfo.threadId = thread.th32ThreadID;
+						threadInfo.processId = thread.th32OwnerProcessID;
+						threadInfo.basePriority = thread.tpBasePri;
+						threadInfo.currentPriority = thread.tpBasePri + thread.tpDeltaPri;
+						threadInfos.Add (threadInfo);
+					}
+					while (NativeMethods.Thread32Next (handleRef, thread));
+				}
+
+				return threadInfos.ToArray ();
+			} finally {
+				if (bufferHandle.IsAllocated)
+					bufferHandle.Free ();
+				//                Debug.WriteLineIf(Process.processTracing.TraceVerbose, "Process - CloseHandle(toolhelp32 snapshot handle)");
+				if (handle != (IntPtr)(-1)) CloseHandle (handle);
+			}
+		}
+
+		[DllImport(ExternDll.Kernel32, ExactSpelling = true, CharSet = System.Runtime.InteropServices.CharSet.Auto, SetLastError = true)]
+		public static extern bool CloseHandle (IntPtr handle);
+
 		[Test]
 		public void BuildConsoleProject ()
 		{
 			var s = Stopwatch.StartNew ();
+			var timer = new System.Threading.Timer (delegate {
+				Console.WriteLine ("Threads count:" + Process.GetCurrentProcess ().Threads.Count + " " +GetProcessInfos ().Length
+//				                   .First (p => p.processId == processId).threadInfoList.Count
+									+ " " + +s.Elapsed.TotalSeconds);
+			}, null, 1000, 1000);
 			string solFile = Path.Combine ("..", "..", "Main.sln");
 
 			Console.WriteLine ("Start:" + s.Elapsed.TotalSeconds);
@@ -62,18 +174,19 @@ namespace MonoDevelop.Projects
 
 			Console.WriteLine ("Roslyn loaded:" + s.Elapsed.TotalSeconds);
 			var compilations = new List<Compilation> ();
-			foreach (var p in solution.Items.OfType<DotNetProject>()) {
+			foreach (var p in solution.Items.OfType<DotNetProject> ()) {
 				compilations.Add (TypeSystemService.GetCompilationAsync (p).Result);
 			}
 			Console.WriteLine ("Compilation done:" + s.Elapsed.TotalSeconds);
 
 			var types = AppDomain.CurrentDomain.GetAssemblies ()
 				.Where (a => !a.IsDynamic)
-				.SelectMany (a => a.GetExportedTypes ())
+				.Where (a => a.FullName.Contains ("NRefactory6"))
+				.SelectMany (a => a.GetTypes ())
 				.Where (t => typeof(DiagnosticAnalyzer).IsAssignableFrom (t))
 				.Where (t => !t.IsInterface
-			            && !t.IsAbstract
-			            && !t.ContainsGenericParameters)
+					&& !t.IsAbstract
+					&& !t.ContainsGenericParameters)
 				.Select (t => (DiagnosticAnalyzer)Activator.CreateInstance (t))
 				.ToImmutableArray ();
 
@@ -91,19 +204,21 @@ namespace MonoDevelop.Projects
 
 			Console.WriteLine ("Diagnostics done:" + s.Elapsed.TotalSeconds);
 
-//			var project = solution.GetAllProjects ().FirstOrDefault ((p) => p.Name == "UnitTests");
-//			var sym = TypeSystemService.GetCompilationAsync (project).Result.GetTypeByMetadataName ("UnitTests.TestBase");
-//			Console.WriteLine ("Find symbol:" + s.Elapsed.TotalSeconds);
-//			var haha = sym.DeclaringSyntaxReferences;
-//			var workspace = TypeSystemService.GetWorkspace (solution);
-//			var references = ((INamedTypeSymbol)sym).FindDerivedClassesAsync (workspace.CurrentSolution).Result.ToArray ();
-//			Console.WriteLine ("Find all references(" + references.Length + "):" + s.Elapsed.TotalSeconds);
+			//			var project = solution.GetAllProjects ().FirstOrDefault ((p) => p.Name == "UnitTests");
+			//			var sym = TypeSystemService.GetCompilationAsync (project).Result.GetTypeByMetadataName ("UnitTests.TestBase");
+			//			Console.WriteLine ("Find symbol:" + s.Elapsed.TotalSeconds);
+			//			var haha = sym.DeclaringSyntaxReferences;
+			//			var workspace = TypeSystemService.GetWorkspace (solution);
+			//			var references = ((INamedTypeSymbol)sym).FindDerivedClassesAsync (workspace.CurrentSolution).Result.ToArray ();
+			//			Console.WriteLine ("Find all references(" + references.Length + "):" + s.Elapsed.TotalSeconds);
 
 			TypeSystemService.Unload (solution);
 
 			s.Stop ();
+			timer.Dispose ();
 			Console.WriteLine ("Finished:" + s.Elapsed.TotalSeconds);
 		}
+
 	}
 }
 
